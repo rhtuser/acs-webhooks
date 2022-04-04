67fd411f8c173b79d341925215287856593b396b
Grega Bremec <gregab@p0f.net>
2022-04-04 15:31:57 +0200

    logging pre- and post-conversion

--- src/main/java/net/p0f/quarkus/acs/webhooks/Endpoints.java
+++ src/main/java/net/p0f/quarkus/acs/webhooks/Endpoints.java
@@ -6,6 +6,8 @@ import org.apache.camel.model.rest.RestBindingMode;
 
 public class Endpoints extends RouteBuilder {
     public void configure() {
+        // TODO: configure route timeout
+
         restConfiguration()
             .component("platform-http")
             .contextPath("/api")
@@ -22,16 +24,21 @@ public class Endpoints extends RouteBuilder {
                 .to("direct:auditLog");
 
         from("direct:unknownPayload")
+            .log(LoggingLevel.INFO, "unknownPayload", "Generic message received.")
+            .to("log:unknownPayload?level=INFO&showBody=true")
             .convertBodyTo(String.class)
-            .log(LoggingLevel.INFO, "unknownPayload", "Generic message received: ${body}");
+            .to("log:unknownPayload?level=INFO&showBody=true");
 
         from("direct:alertLog")
+            .log(LoggingLevel.INFO, "alertLog", "Alert message received")
+            .to("log:alertLog?level=INFO&showBody=true")
             .convertBodyTo(String.class)
-            .log(LoggingLevel.INFO, "alertLog", "Alert message received: ${body}");
+            .to("log:alertLog?level=INFO&showBody=true");
 
         from("direct:auditLog")
+            .log(LoggingLevel.INFO, "auditLog", "Audit message received")
+            .to("log:auditLog?level=INFO&showBody=true")
             .convertBodyTo(String.class)
-            .log(LoggingLevel.INFO, "auditLog", "Audit message received: ${body}");
-
+            .to("log:auditLog?level=INFO&showBody=true");
     }
 }
